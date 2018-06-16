98582937cf2838c4fb5045307c0b1f78cb816243
Anders Conbere <anders@stripe.com>
2018-06-15 21:55:01 -0700

    intr

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+extern crate sdl2;
+
 mod bytes;
 mod registers;
 mod instructions;
 mod cpu;
 mod mmu;
 mod device;
+mod display;
 
 fn main() {
     let mut registers = registers::new();
     let instructions = instructions::new();
     let cartridge = device::cartridge::load_from_file(String::from("/Users/anders/Projects/gb_test_roms/sheepitup.gb"));
+    let display = display::new();
     let mut mmu = mmu::new(cartridge);
     let mut cpu = cpu::new(&mut registers, &instructions, &mut mmu);
     cpu.run();
